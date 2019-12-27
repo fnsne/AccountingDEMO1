2773d0914ada017c5968620d048df0f577246a72
fnsne <watashiwafnsne@gmail.com>
2019-12-27 13:19:28 +0800

    inline getBudgets()

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -14,7 +14,16 @@ public class Accounting {
 
     public double QueryBudget(LocalDate start, LocalDate end) {
         double sum = 0;
-        for (Budget budget : getBudgets(start, end)) {
+        List<Budget> totalBudgets = new ArrayList<>();
+        for (Budget budget1 : db.GetAll()) {
+            if ((budget1.getYearMonth().equals(YearMonth.from(start)) || budget1.getYearMonth().isAfter(YearMonth.from(start)))
+                    &&
+                    (budget1.getYearMonth().equals(YearMonth.from(end)) || budget1.getYearMonth().isBefore(YearMonth.from(end)))) {
+                totalBudgets.add(budget1);
+            }
+        }
+        List<Budget> budgets = totalBudgets;
+        for (Budget budget : budgets) {
             double budgetAmount = overlappingAmount(start, end, budget);
             sum += budgetAmount;
         }
@@ -35,18 +44,6 @@ public class Accounting {
         periodStartDay = period1.getStart().isAfter(period2.getStart()) ? period1.getStart() : period2.getStart();
         periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
         return new Period(periodStartDay, periodEndDay);
-    }
-
-    private List<Budget> getBudgets(LocalDate start, LocalDate end) {
-        List<Budget> totalBudgets = new ArrayList<>();
-        for (Budget budget : db.GetAll()) {
-            if ((budget.getYearMonth().equals(YearMonth.from(start)) || budget.getYearMonth().isAfter(YearMonth.from(start)))
-                    &&
-                    (budget.getYearMonth().equals(YearMonth.from(end)) || budget.getYearMonth().isBefore(YearMonth.from(end)))) {
-                totalBudgets.add(budget);
-            }
-        }
-        return totalBudgets;
     }
 
     private boolean InSameMonth(LocalDate start, LocalDate end) {
