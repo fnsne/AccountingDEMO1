6e9c773bdc695a75293509af52f6c36744917b5f
fnsne <watashiwafnsne@gmail.com>
2019-12-27 15:10:19 +0800

    把不相交的判斷，改成用Period(start, end)的寫法

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -12,7 +12,7 @@ public class Accounting {
     public double QueryBudget(LocalDate start, LocalDate end) {
         double sum = 0;
         for (Budget budget : db.GetAll()) {
-            double diff = overlappingDays(start, end, budget);
+            double diff = overlappingDays(budget, new Period(start, end));
             double budgetAmount = diff * budget.getDailyAmount();
             sum += budgetAmount;
         }
@@ -20,19 +20,20 @@ public class Accounting {
 
     }
 
-    private double overlappingDays(LocalDate start, LocalDate end, Budget budget) {
+    private double overlappingDays(Budget budget, Period period) {
         double diff;
-        if (start.isAfter(budget.lastDay()) || end.isBefore(budget.firstDay())) {
+        LocalDate start1 = period.getStart();
+        LocalDate end1 = period.getEnd();
+        if (start1.isAfter(budget.lastDay()) || end1.isBefore(budget.firstDay())) {
             diff = 0;
         } else {
-            Period period1 = new Period(start, end);
+            Period period1 = period;
             Period period2 = budget.createPeriod();
             LocalDate periodStartDay;
             LocalDate periodEndDay;
             periodStartDay = period1.getStart().isAfter(period2.getStart()) ? period1.getStart() : period2.getStart();
             periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
-            Period period = new Period(periodStartDay, periodEndDay);
-            diff = period.getDays();
+            diff = new Period(periodStartDay, periodEndDay).getDays();
         }
         return diff;
     }
