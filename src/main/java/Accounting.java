cd3cd8ee86d28e49a715ec2d1b47ccc76b43db94
fnsne <watashiwafnsne@gmail.com>
2019-12-27 14:49:07 +0800

    inline overlappingAmount

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -1,8 +1,5 @@
 import java.time.LocalDate;
-import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Accounting {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
@@ -18,19 +15,15 @@ public class Accounting {
             double budgetAmount = 0;
             if (start.isAfter(budget.lastDay()) || end.isBefore(budget.firstDay())) {
             } else {
-                budgetAmount = overlappingAmount(start, end, budget);
+                Period period1 = new Period(start, end);
+                Period period2 = budget.createPeriod();
+                Period period = overlappingPeriod(period1, period2);
+                budgetAmount = budget.budgetAmountOfPeriod(period);
             }
             sum += budgetAmount;
         }
         return sum;
 
-    }
-
-    private double overlappingAmount(LocalDate start, LocalDate end, Budget budget) {
-        Period period1 = new Period(start, end);
-        Period period2 = budget.createPeriod();
-        Period period = overlappingPeriod(period1, period2);
-        return budget.budgetAmountOfPeriod(period);
     }
 
     private Period overlappingPeriod(Period period1, Period period2) {
