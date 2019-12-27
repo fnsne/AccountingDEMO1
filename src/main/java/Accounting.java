b2c49c51579c4287e186a19dc3559a294f230589
fnsne <watashiwafnsne@gmail.com>
2019-12-27 15:00:45 +0800

    extract method overlappingDays

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -14,23 +14,29 @@ public class Accounting {
         for (Budget budget : db.GetAll()) {
             double budgetAmount = 0;
             double diff = 0;
-            if (start.isAfter(budget.lastDay()) || end.isBefore(budget.firstDay())) {
-                diff = 0;
-            } else {
-                Period period1 = new Period(start, end);
-                Period period2 = budget.createPeriod();
-                LocalDate periodStartDay;
-                LocalDate periodEndDay;
-                periodStartDay = period1.getStart().isAfter(period2.getStart()) ? period1.getStart() : period2.getStart();
-                periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
-                Period period = new Period(periodStartDay, periodEndDay);
-                diff = period.getDays();
-            }
+            diff = overlappingDays(start, end, budget);
             budgetAmount = diff * budget.getDailyAmount();
             sum += budgetAmount;
         }
         return sum;
 
+    }
+
+    private double overlappingDays(LocalDate start, LocalDate end, Budget budget) {
+        double diff;
+        if (start.isAfter(budget.lastDay()) || end.isBefore(budget.firstDay())) {
+            diff = 0;
+        } else {
+            Period period1 = new Period(start, end);
+            Period period2 = budget.createPeriod();
+            LocalDate periodStartDay;
+            LocalDate periodEndDay;
+            periodStartDay = period1.getStart().isAfter(period2.getStart()) ? period1.getStart() : period2.getStart();
+            periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
+            Period period = new Period(periodStartDay, periodEndDay);
+            diff = period.getDays();
+        }
+        return diff;
     }
 
     private boolean InSameMonth(LocalDate start, LocalDate end) {
