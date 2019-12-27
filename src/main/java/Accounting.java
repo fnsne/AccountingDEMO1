fc9d1789eb89cfb66c5e9f3042dde655538f7548
fnsne <watashiwafnsne@gmail.com>
2019-12-27 13:02:38 +0800

    inline getOverlappingPeriod

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -23,29 +23,27 @@ public class Accounting {
         } else {
             double sum = 0;
             for (Budget budget : getBudgets(start, end)) {
-                Period period = getOverlappingPeriod(new Period(start, end), budget.createPeriod());
+                Period period1 = new Period(start, end);
+                Period period2 = budget.createPeriod();
+                LocalDate periodStartDay;
+                LocalDate periodEndDay;
+                if (InSameMonth(period1.getStart(), period2.getStart())) {
+                    periodStartDay = period1.getStart().isAfter(period2.getStart()) ? period1.getStart() : period2.getStart();
+                    periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
+                } else if (InSameMonth(period1.getEnd(), period2.getStart())) {
+                    periodStartDay = period2.getStart();
+                    periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
+                } else {
+                    periodStartDay = period2.getStart();
+                    periodEndDay = period2.getEnd();
+                }
+                Period period = new Period(periodStartDay, periodEndDay);
                 double budgetAmount = budget.budgetAmountOfPeriod(period);
                 sum += budgetAmount;
             }
             return sum;
         }
 
-    }
-
-    private Period getOverlappingPeriod(Period period1, Period period2) {
-        LocalDate periodStartDay;
-        LocalDate periodEndDay;
-        if (InSameMonth(period1.getStart(), period2.getStart())) {
-            periodStartDay = period1.getStart().isAfter(period2.getStart()) ? period1.getStart() : period2.getStart();
-            periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
-        } else if (InSameMonth(period1.getEnd(), period2.getStart())) {
-            periodStartDay = period2.getStart();
-            periodEndDay = period1.getEnd().isBefore(period2.getEnd()) ? period1.getEnd() : period2.getEnd();
-        } else {
-            periodStartDay = period2.getStart();
-            periodEndDay = period2.getEnd();
-        }
-        return new Period(periodStartDay, periodEndDay);
     }
 
     private List<Budget> getBudgets(LocalDate start, LocalDate end) {
